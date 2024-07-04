cfda1a65c74d260a1e7a9354f1e9a012982df541
Nikhil Sharma <nikhilsharmalku@gmail.com>
2024-07-04 20:14:31 +0530

    04.07.2024.c1
    4th July
    commit 1

---
+++ py.py
@@ -0,0 +1,4 @@
+a=[2,3]
+b=[4,5]
+x=list(zip(a,b))
+print(x)
--- script.js
+++ script.js
@@ -1,11 +1,3 @@
 // _ _$&;
-// let o = {
-//     name: "Nikhil",
-//     "branch name": "Mechanical"
-// }
-
-// o.salary = "100crs";
-
-for (let i = 0; i < 10; i++) {
-    console.log(1+i);
-}
+let arr = [1, 5, 6];
+console.log(arr)
